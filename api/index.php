e0e5e9402f619869de1855c3a8a4499a4ce95471
angelicaptr <angelicaputrias@gmail.com>
2026-08-14 02:00:53 +0700

    fix: use modern vercel rewrites and independent lambda bootloader

--- api/index.php
+++ api/index.php
@@ -32,4 +32,16 @@ unset($_ENV['APP_PACKAGES_CACHE']);
 unset($_ENV['APP_ROUTES_CACHE']);
 
 // 4. Jalankan Laravel
-require __DIR__ . '/../public/index.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
