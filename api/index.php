0c1eb968b829ec71c3d87ddfcec809e385447a6b
angelicaptr <angelicaputrias@gmail.com>
2026-08-13 17:35:58 +0700

    fix: disable Laravel caching in Vercel API entrypoint

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
 <?php
 
+// Paksa Laravel menggunakan folder /tmp untuk compiled views dan cache di Vercel
+unset($_ENV['APP_CONFIG_CACHE']);
+unset($_ENV['APP_SERVICES_CACHE']);
+unset($_ENV['APP_PACKAGES_CACHE']);
+unset($_ENV['APP_ROUTES_CACHE']);
+
 require __DIR__ . '/../public/index.php';
