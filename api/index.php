2305aaccd1a102dc2fb29ae9ce9ef7a6e4feb19d
angelicaptr <angelicaputrias@gmail.com>
2026-08-14 02:50:08 +0700

    chore: force debug mode and stderr logging on vercel

--- api/index.php
+++ api/index.php
@@ -22,6 +22,12 @@ $_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
 $_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
 
+// PAKSA Mode Debug agar kita bisa melihat error aslinya!
+$_ENV['APP_DEBUG'] = 'true';
+putenv('APP_DEBUG=true');
+$_ENV['LOG_CHANNEL'] = 'stderr';
+putenv('LOG_CHANNEL=stderr');
+
 // 3. Matikan penulisan cache otomatis yang bikin crash
 unset($_ENV['APP_CONFIG_CACHE']);
 unset($_ENV['APP_SERVICES_CACHE']);
